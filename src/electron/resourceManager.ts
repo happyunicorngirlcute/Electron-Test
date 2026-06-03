25fd137d3fd41ae320eb3656d1ca6a118c535ea9
nahildiabi8-stack <nahildiabi8@gmail.com>
2026-06-03 12:22:41 +0200

    Added getStorage function, Will log how much storage you have left in your computer that's free

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,4 +1,5 @@
 import osUtils from "os-utils";
+import fs from 'fs';
 
 const POLLING_INTERVAL = 500;
 
@@ -18,4 +19,16 @@ function getCpuUsage() {
 
 function getRamUsage() {
   return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  
+  const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/');
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+
+  return {
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
 }
