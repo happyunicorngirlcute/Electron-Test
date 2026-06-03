b624becf0a685980eae7edb500c837fc40988b20
nahildiabi8-stack <nahildiabi8@gmail.com>
2026-06-03 12:04:54 +0200

    Added a polling loop, every 500 milliseconds, logs the cpu usage using os utils

--- src/electron/ressourceManager.tsx
+++ src/electron/ressourceManager.tsx
@@ -1,5 +1,13 @@
+import osUtils from 'os-utils';
+
 const POLLING_INTERVAL = 500;
 
 export function pollResources() {
-  setInterval(() => {}, POLLING_INTERVAL);
+  setInterval(() => {
+    getCpuUsage();
+  }, POLLING_INTERVAL);
+}
+
+function getCpuUsage() {
+  osUtils.cpuUsage((percentage) => console.log(percentage));
 }
