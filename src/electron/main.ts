ff6f7aa9bc48f0885af3fb8f6b07790de4cb4bf5
nahildiabi8-stack <nahildiabi8@gmail.com>
2026-06-03 14:30:52 +0200

    Added path resolver, will finish preload

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,14 +1,16 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
-import { isDev } from './util.js';
-import { pollResources } from './resourceManager.js';
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+import { pollResources } from "./resourceManager.js";
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {},
+  });
   if (isDev()) {
-    mainWindow.loadURL('http://localhost:5123');
+    mainWindow.loadURL("http://localhost:5123");
   } else {
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
 
   pollResources();
