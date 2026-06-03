93d610593ced10223beb546085cac94a3612269d
nahildiabi8-stack <nahildiabi8@gmail.com>
2026-06-03 10:20:10 +0200

    Adding typescript

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,10 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
-
 // type test = string;
 // const myMessage: test = 'Electron app is running!';
-
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
 });
