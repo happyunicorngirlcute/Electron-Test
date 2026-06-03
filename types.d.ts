962663763fac32aec202dab0e7ed9cb5e29add25
nahildiabi8-stack <nahildiabi8@gmail.com>
2026-06-03 15:22:50 +0200

    Updated json, typescript now knows what is electron

---
+++ types.d.ts
@@ -0,0 +1,21 @@
+type Statistics = {
+  cpuUsage: number;
+  ramUsage: number;
+  storageData: {
+    usage: number;
+  };
+};
+
+
+type StaticData = {
+  totalStorage: number;
+  cpuModel: string;
+  totalMemoryGB: number;
+};
+
+interface Window {
+    electron: {
+        subscribeStatistics: (callback: (statistics: Statistics) => void) => void;
+        getStaticData: () => Promise<StaticData>;
+    };
+}
