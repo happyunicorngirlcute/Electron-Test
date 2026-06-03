a13e01ad71c2c61ed96ee7c2c9721fd4ab394176
nahildiabi8-stack <nahildiabi8@gmail.com>
2026-06-03 16:19:31 +0200

    Update utils.ts, making sure only strings are allowed

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,3 +1,12 @@
+import { ipcMain } from "electron";
+import { getStaticData } from "./resourceManager.js";
+
 export function isDev(): boolean {
-  return process.env.NODE_ENV === 'development';
+  return process.env.NODE_ENV === "development";
+}
+
+function ipcHandle<Key extends string>(key: Key, handler: () => any) {
+  ipcMain.handle(key, () => handler());
+  {
+  }
 }
